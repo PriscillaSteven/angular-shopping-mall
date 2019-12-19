ba06e71633d476cd101fc43384d0a57dd8930602
PriscillaSteven <1130496332@qq.com>
2019-12-19 19:18:42 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {}
+export class AppComponent  {
+  constructor(){
+    console.log("Is this production environment? " + environment.production); // logs false for default environment
+  }
+  title = 'app works!';
+}
 
 
 /*
